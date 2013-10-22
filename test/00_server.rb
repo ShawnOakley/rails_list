391eeb3cb337054a13c5c6924c8a4f0b014981a6
Shawn <shawnoakley@gmail.com>
2013-10-22 14:10:17 -0400

    Reinitializes repo

---
+++ test/00_server.rb
@@ -0,0 +1,22 @@
+require 'active_support/core_ext'
+require 'webrick'
+require 'rails_lite'
+
+server = WEBrick::HTTPServer.new :Port => 8080
+trap('INT') { server.shutdown }
+
+class MyController < ControllerBase
+  def go
+  render_content("hello world!", "text/html")
+   render :show
+   session["count"] ||= 0
+   session["count"] += 1
+   render :counting_show
+  end
+end
+
+server.mount_proc '/' do |req, res|
+  MyController.new(req, res).go
+end
+
+server.start
---
+++ test/01_router_server.rb
@@ -0,0 +1,41 @@
+require 'active_support/core_ext'
+require 'json'
+require 'webrick'
+require 'rails_lite'
+
+server = WEBrick::HTTPServer.new :Port => 8080
+trap('INT') { server.shutdown }
+
+class StatusController < ControllerBase
+  def index
+    statuses = ["s1", "s2", "s3"]
+
+    render_content(statuses.to_json, "text/json")
+  end
+
+  def show
+    render_content("status ##{params[:id]}", "text/text")
+  end
+end
+
+class UserController < ControllerBase
+  def index
+    users = ["u1", "u2", "u3"]
+
+    render_content(users.to_json, "text/json")
+  end
+end
+
+server.mount_proc '/' do |req, res|
+  router = Router.new
+  router.draw do
+    get Regexp.new("^/statuses$"), StatusController, :index
+    get Regexp.new("^/users$"), UserController, :index
+
+   get Regexp.new("^/statuses/(?<id>\\d+)$"), StatusController, :show
+  end
+
+  route = router.run(req, res)
+end
+
+server.start
